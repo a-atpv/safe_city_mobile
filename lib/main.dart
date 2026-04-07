0c33cf4b3bbdc526944ce0ff2ce8c1af0f0febf3
a.atpv <aldiyar09.02@mail.ru>
2026-04-07 16:19:44 +0500

    refactor: remove Firebase and push notification service

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'core/theme/theme.dart';
 import 'core/router/app_router.dart';
-import 'core/services/push_notification_service.dart';
-// import 'firebase_options.dart'; // Uncomment after running `flutterfire configure`
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  try {
-    // Initialize Firebase
-    await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform, // Uncomment after running `flutterfire configure`
-    );
-    
-    // Initialize Push Notifications
-    final pushService = PushNotificationService();
-    await pushService.initialize();
-  } catch (e) {
-    debugPrint('Firebase initialization failed. Did you run flutterfire configure? Error: $e');
-  }
+
   
   // Set status bar style
   SystemChrome.setSystemUIOverlayStyle(
